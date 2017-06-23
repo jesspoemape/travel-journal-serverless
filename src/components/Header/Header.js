eb21899117e97984f9263c08d9e46b3e65fe9519
Jess Poemape <jesspoemape@gmail.com>
2017-06-23 16:37:08 -0600

    moon phase icon is sized correctly. mobile logo visible. desktop logo hidden.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,8 @@ import {Link} from 'react-router-dom';
 const Header = () => {
     return (
         <div className='header-container'>
+            <h1 className='logo-name-desktop'>wandr</h1>
+            <Link className='link' to='/'><h1 className='logo-letter-mobile'>w</h1></Link>
             <Link to='/create-post'>
                 <div className='create-post-btn'>
                     <h1>+</h1>
