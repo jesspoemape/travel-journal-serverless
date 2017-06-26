43dadde733fe43ec1c8fd92cbea8878d17b0a7d9
Jess Poemape <jesspoemape@gmail.com>
2017-06-26 14:56:05 -0600

    simplified parallax css. no registreed service worker.

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {Provider} from 'react-redux';
 import store from './store';
 
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 
